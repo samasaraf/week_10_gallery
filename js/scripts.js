c5775543961bf23f53e825ffc810e752c5db0d76
Sama Saraf <samasaraf@Samas-MacBook-Pro.local>
2020-12-12 22:57:59 -0800

    template

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,33 @@
+console.log('Hello from js/scripts.js!');
 
-$(document).ready(function() {
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
 
-    var content = $('.content');
-    var currentItem = content.filter('.active');
-    var steps = $('.card').filter('.steps');
-    var inactive1 = $('.inactive-1');
-    var inactive2 = $('.inactive-2');
 
-    $('.button').click(function() {
-        var nextItem = currentItem.next();
-        var lastItem = content.last();
-        var contentFirst = content.first();
+$(document).ready(function(){
+  // Activate Carousel
+  $("#myCarousel").carousel();
 
-        currentItem.removeClass('active');
+  // Enable Carousel Indicators
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel").carousel(2);
+  });
+  $(".item4").click(function(){
+    $("#myCarousel").carousel(3);
+  });
 
-        if (currentItem.is(lastItem)) {
-            currentItem = contentFirst.addClass('active');
-            currentItem.css({'right': '10%', 'opacity': '1'});
-            $('.step').animate({width: '33%'});
-            inactive1.animate({height: '8px', marginLeft:'20px', marginRight:'20px'}, 100);
-            inactive2.animate({height: '8px', marginLeft:'10px', marginRight:'10px'}, 100);
-
-        } else if (currentItem.is(contentFirst)) {
-            currentItem.animate({opacity: 0}, 1000);
-            currentItem = nextItem.addClass('active');
-            $('.step').animate({width: '66%'});
-            inactive2.animate({height: '0', marginLeft:'0px', marginRight:'0px'}, 100);
-
-        } else {
-            currentItem = nextItem.addClass('active');
-            $('.step').animate({width: '100%'});
-            inactive1.animate({height: '0', marginLeft:'0px', marginRight:'0px'}, 100);
-        }
-    });
-
-});
+  // Enable Carousel Controls
+  $(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+})
